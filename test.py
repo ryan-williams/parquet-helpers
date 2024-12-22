34d2b1ddc93f3a3cd04270268338c41309e41fa3
Ryan Williams <ryan@runsascoded.com>
2024-12-22 14:08:07 -0500

    `test.parquet`: make "Gender" a categorical

--- test.py
+++ test.py
@@ -3,5 +3,6 @@
 import pandas as pd
 
 df = pd.read_parquet("test.parquet")
-df = df.astype({'Gender': 'Int8'})
+gender_map = { 0: "U", 1: "M", 2: "F" }
+df["Gender"] = df["Gender"].map(gender_map).astype("category")
 df.to_parquet('test.parquet')
