9a9370c2bd024bc3dc9bb4fcf53626635c5002ed
Ryan Williams <ryan@runsascoded.com>
2024-12-24 22:12:13 -0500

    drop "Gender", sort rows by "Ride ID", sort columns

--- test.py
+++ test.py
@@ -3,6 +3,6 @@
 import pandas as pd
 
 df = pd.read_parquet("test.parquet")
-gender_map = { 0: "U", 1: "M", 2: "F" }
-df["Gender"] = df["Gender"].map(gender_map).astype("category")
-df.to_parquet('test.parquet')
+df = df.drop(columns='Gender').sort_values('Ride ID')
+df = df[sorted(df.columns)]
+df.to_parquet('test.parquet', index=False)
