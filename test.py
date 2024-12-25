583d014e024509e84c098886e9a087bbe81f449a
Ryan Williams <ryan@runsascoded.com>
2024-12-25 15:05:40 -0500

    `test.py`: rename `test{2,}.parquet`, "Stop Time" to "End Time"

--- test.py
+++ test.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 
-df = pd.read_parquet("test.parquet")
-df = df.drop(columns='Gender').sort_values('Ride ID')
-df = df[sorted(df.columns)]
+df = pd.read_parquet("test2.parquet")
+df = df.rename(columns={'Stop Time': 'End Time'})
 df.to_parquet('test.parquet', index=False)
